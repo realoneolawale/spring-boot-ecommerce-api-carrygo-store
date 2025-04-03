cd1de8529624af2a40e4d6054ec4e144ef5bdde5
Amoo Olawale <realoneolawale@outlook.com>
2025-04-03 23:30:15 +0100

    Added Role entity to User enity

---
+++ src/main/java/com/techqwerty/spring_boot_ecommerce_api_carrygo_store/entities/Role.java
@@ -0,0 +1,20 @@
+package com.techqwerty.spring_boot_ecommerce_api_carrygo_store.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
--- src/main/java/com/techqwerty/spring_boot_ecommerce_api_carrygo_store/entities/User.java
+++ src/main/java/com/techqwerty/spring_boot_ecommerce_api_carrygo_store/entities/User.java
@@ -5,9 +5,13 @@ import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -39,4 +43,9 @@ public class User {
     // user has a list of orders
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private Set<Order> orders;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
+
 }
